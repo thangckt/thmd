fa4a8e01d765ddeb1dd8dea16151feb162dfc6a4
thangckt <thangckt@gmail.com>
2026-07-22 04:25:03 +0000

    Update from https://github.com/thangckt/src_thmd/commit/9446055c12665d6fc50a39e500eb696509e16f49

--- 1thang_js/page_logVisitor.js
+++ 1thang_js/page_logVisitor.js
@@ -326,8 +326,8 @@
     // Filter visitors based on IP, ASN, or other fields
     const blackList1 = [ // Define blacklist list-of-dictionaries
         { ip: '114.70.12.225', org: 'Ulsan National Institute of Science and Technology', os: 'Linux-Unk' }, // CANLab desktop
-        { ip: '180.228.220.66', org: 'LG POWERCOMM', os: 'Linux-Unk' }, // Home laptop
-        { ip: '180.228.220.66', org: 'LG POWERCOMM', os: 'Android-10' }, // Home mobile
+        { ip: '2603:8080:b201:2b73::13de', org: 'Charter Communications Inc', os: 'Linux-Unk' }, // Home laptop
+        { ip: '2603:8080:b201:2b73::13de', org: 'Charter Communications Inc', os: 'Android-10' }, // Home mobile
     ];
 
     function checkIfBlocked(visitorInfo, blackList) {
